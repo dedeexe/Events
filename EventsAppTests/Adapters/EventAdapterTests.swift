3c33277ae26de854b3f0c3a44a34862bf4050c45
Fabricio Dede <dede.exe@gmail.com>
2020-10-01 22:46:04 -0300

    add: Tests for EventAdapter

--- EventsAppTests/Adapters/EventAdapterTests.swift
+++ EventsAppTests/Adapters/EventAdapterTests.swift
@@ -1,28 +1,60 @@
 import XCTest
 import HexagonEdges
+import Combine
 @testable import EventsApp
 
 class EventAdapterTests: XCTestCase {
 
     var sut: EventsAdapter!
     
-    override func setUpWithError() throws {
-        
-    }
+    var cancellables: Set<AnyCancellable> = []
 
-    override func tearDownWithError() throws {
+    func testGetAllEventsSuccess() throws {
+        sut = EventsAdapterWorker(service: MockedEventsService(isFail: false))
+        let expectation = XCTestExpectation()
         
+        sut.allEvents().sink(
+            receiveCompletion: { _ in },
+            receiveValue: { events in
+                XCTAssertEqual(events.count, 3)
+                checkEvent(events.first!)
+                checkPerson(events.first!.people.first!)
+                expectation.fulfill()
+            }
+        )
+        .store(in: &cancellables)
+            
+        wait(for: [expectation], timeout: 5.0)
     }
-
-    func testAllEventsSuccess() throws {
+    
+    func testGetAllEventsFail() throws {
+        sut = EventsAdapterWorker(service: MockedEventsService(isFail: true))
+        let expectation = XCTestExpectation()
         
+        sut.allEvents().sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error as NSError):
+                    XCTAssertEqual(error.code, 1000)
+                    XCTAssertEqual(error.domain, "TestError")
+                    expectation.fulfill()
+                    
+                default:
+                    break
+                }
+            },
+            receiveValue: { _ in }
+        )
+        .store(in: &cancellables)
+            
+        wait(for: [expectation], timeout: 5.0)
     }
 }
 
 
 fileprivate class MockedEventsService: EventsService {
     let isFail: Bool
-    let error = NSError(domain: "TestErrir", code: 1000, userInfo: nil)
+    let error = NSError(domain: "TestError", code: 1000, userInfo: nil)
     
     init(isFail: Bool) {
         self.isFail = isFail
@@ -35,16 +67,26 @@ fileprivate class MockedEventsService: EventsService {
             return
         }
         
-        
+        let events = try! JSONDecoder().decode([InputModel.Event].self, from: Fixture.EventList.data)
+        let result = DecodedResult<[InputModel.Event]>.success(events)
+        completion(result)
     }
     
-    func getEvent(id: String, completion: @escaping DecodedCompletion<InputModel.Event>) {
-        
-    }
-    
-    let mockEvents: [InputModel.Event] = [
-    
-    ]
+    func getEvent(id: String, completion: @escaping DecodedCompletion<InputModel.Event>) {}
 }
 
+func checkEvent(_ model: Event) {
+    XCTAssertEqual(model.description, "description")
+    XCTAssertEqual(model.image, "event.png")
+    XCTAssertEqual(model.longitude, -1.1)
+    XCTAssertEqual(model.latitude, -2.2)
+    XCTAssertEqual(model.title, "Event Test")
+    XCTAssertEqual(model.id, "1")
+}
 
+func checkPerson(_ model: Person) {
+    XCTAssertEqual(model.picture, "person.png")
+    XCTAssertEqual(model.name, "Person1")
+    XCTAssertEqual(model.eventId, "1")
+    XCTAssertEqual(model.id, "1")
+}
---
+++ EventsAppTests/Tests_Helpers/Fixtures.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum Fixture {
+    struct Event {
+        static let data = Event.json.data(using: .utf8) ?? Data(capacity: 0)
+        static let json = #"""
+
+        {"people":[{"picture":"person.png","name":"Person1","eventId":"1","id":"1"}],"date":12345,"description":"description","image":"event.png","longitude":"-1.1","latitude":"-2.2","price":12.34,"title":"Event Test","id":"1"}
+
+        """#
+    }
+    
+    struct EventList {
+        static let data = EventList.json.data(using: .utf8) ?? Data(capacity: 0)
+        static let json = #"""
+        [{"people":[{"picture":"person.png","name":"Person1","eventId":"1","id":"1"}],"date":12345,"description":"description","image":"event.png","longitude":-1.1,"latitude":-2.2,"price":12.34,"title":"Event Test","id":"1"}, {"people":[{"picture":"person.png","name":"Person1","eventId":"1","id":"1"}],"date":12345,"description":"description","image":"event.png","longitude":-1.1,"latitude":-2.2,"price":12.34,"title":"Event Test","id":"1"}, {"people":[{"picture":"person.png","name":"Person1","eventId":"1","id":"1"}],"date":12345,"description":"description","image":"event.png","longitude":-1.1,"latitude":-2.2,"price":12.34,"title":"Event Test","id":"1"}]
+        """#
+    }
+}
